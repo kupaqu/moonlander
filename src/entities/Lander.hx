c714e4b1b9e67845715c81b037447af67a480c56
Buda Vampilov <budavampilov@MacBook-Air-Buda.local>
2023-01-04 04:18:17 +0800

    some changes

--- src/entities/Lander.hx
+++ src/entities/Lander.hx
@@ -7,6 +7,10 @@ class Lander extends h2d.Bitmap {
     var vy: Float;
     var vx: Float;
 
+    var poly: h2d.col.Polygon;
+
+    var p: h2d.Graphics;
+
     var engineAcceleration: Float = 9.8*20;
 
     public function new(parent: Screen) {
@@ -15,6 +19,15 @@ class Lander extends h2d.Bitmap {
         tile.dy -= tile.height/2;
         x = hxd.Window.getInstance().width / 2;
         y = hxd.Window.getInstance().height / 2;
+
+        var b = getBounds();
+        poly = new h2d.col.Polygon([
+            new h2d.col.Point(b.x - x, b.y - y),
+            new h2d.col.Point(b.xMax - x, b.y - y),
+            new h2d.col.Point(b.xMax - x, b.yMax - y),
+            new h2d.col.Point(b.x - x, b.yMax - y)
+        ]);
+        p = new h2d.Graphics(parent);
     }
 
     public function update(dt: Float) {
@@ -25,9 +38,20 @@ class Lander extends h2d.Bitmap {
             vy -= Math.sin(rotation+Math.PI/2)*engineAcceleration*dt;
             vx -= Math.cos(rotation+Math.PI/2)*engineAcceleration*dt;
         }
-        if (hxd.Key.isPressed(hxd.Key.RIGHT)) rotate(0.01);
-        if (hxd.Key.isPressed(hxd.Key.LEFT)) rotate(-0.01);
-
+        if (hxd.Key.isPressed(hxd.Key.RIGHT)) {
+            rotate(0.01);
+            for (point in poly.points) point.rotate(0.01);
+        }
+        if (hxd.Key.isPressed(hxd.Key.LEFT)) {
+            rotate(-0.01);
+            for (point in poly.points) point.rotate(-0.01);
+        }
         vy += g*dt;
+        
+        p.clear();
+        p.beginFill(0xD1CBCB);
+        for (point in poly.points) {
+            p.drawCircle(point.x + x, point.y + y, 2);
+        }
     }
 }
